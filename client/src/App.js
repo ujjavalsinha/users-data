77b7d9fc64c02d2511d2ee58709359373e4c65f4
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-09-29 18:10:39 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserTable from './components/UserTable/UserTable';
+
+const App = props => {
+  return (
+    <div className='App'>
+      <h1>USER DATA</h1>
+      <UserTable />
+    </div>
+  )
+}
+export default App;
